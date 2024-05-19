d3ab45b096d563a71399029bc5f1110d0cdf17ed
mvkozak-fe <mvkozak.fe@gmail.com>
2024-05-20 00:05:09 +0300

    add opposite-number task

---
+++ opposite-number.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+//SOLUTION
+
+function opposite(number) {
+    return(-number);
+}
+console.log(opposite(10))
