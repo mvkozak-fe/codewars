47eaf201c767c8336379016a9437c5ea2924a734
mvkozak-fe <mvkozak.fe@gmail.com>
2024-05-20 11:09:38 +0300

    add reversed-Strings task

---
+++ reversed-Strings.js
@@ -0,0 +1,7 @@
+// DESCRIPTION:
+// Complete the solution so that it reverses the string passed into it.
+//SOLUTION:
+function solution(str){
+    return str.split('').reverse().join('');  
+}
+console.log(solution('WORLD'))
