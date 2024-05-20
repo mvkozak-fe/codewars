2454d3a84c8990cc34458fcda97f4601d721cf9f
mvkozak-fe <mvkozak.fe@gmail.com>
2024-05-20 12:29:20 +0300

    add growth-of-a-Population task

---
+++ growth-of-a-Population.js
@@ -0,0 +1,13 @@
+// DESCRIPTION:
+// In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater than or equal to p = 1200 inhabitants?
+//SOLUTION:
+function nbYear(p0, percent, aug, p) {
+    // your code    
+    let count = 0;
+    let res = 0;
+    while (p0 < p) {
+    	p0 = p0 + Math.floor(p0 * percent/100) + aug;
+      count++;
+    }
+    return count
+}
