7b6191f0232dbf56d20b36d9939b9b44a2416304
mvkozak-fe <mvkozak.fe@gmail.com>
2024-05-21 21:26:16 +0300

    add countOddNumbersbelown task

---
+++ countOddNumbersbelown.js
@@ -0,0 +1,7 @@
+// DESCRIPTION:
+// Given a number n, return the number of positive odd numbers below n, EASY!
+//SOLUTION:
+function oddCount(n){
+    return Math.floor(n/2);
+}
+console.log(oddCount(5))
