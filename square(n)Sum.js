37d923cb4b45d56e79352bdecc7a42ba8d9a6dda
mvkozak-fe <mvkozak.fe@gmail.com>
2024-05-21 21:29:46 +0300

    add square(n)Sum task

---
+++ square(n)Sum.js
@@ -0,0 +1,6 @@
+// DESCRIPTION:
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+//SOLUTION:
+function squareSum(numbers){
+    return numbers.map(el => el* el).reduce((acc, cur)=>acc + cur,0)
+}
