13bf9597aef3a0c08c5d919d10b197ceabf23f09
mvkozak-fe <mvkozak.fe@gmail.com>
2024-05-21 21:27:50 +0300

    add sumTheStrings task

---
+++ sumTheStrings.js
@@ -0,0 +1,6 @@
+// DESCRIPTION:
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+//SOLUTION:
+function sumStr(a,b) {
+    return (+a + +b).toString();
+}
